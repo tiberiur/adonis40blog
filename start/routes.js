1cc2c1f59c057cede4a8431cda003480b88b1746
Tiberiu Rus <tiberiu@tiberiurus.com>
2018-07-24 22:34:22 +0300

    version 0.1

---
+++ start/routes.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+const Route = use('Route')
+
+Route.on('/').render('home');
+
+Route.get('/posts', 'PostController.index')
+Route.get('/posts/add', 'PostController.add')
+Route.get('/posts/edit/:id', 'PostController.edit')
+Route.get('/posts/:id', 'PostController.details')
+Route.post('/posts', 'PostController.store')
+Route.put('/posts/:id', 'PostController.update')
+Route.delete('/posts/:id', 'PostController.destroy')
